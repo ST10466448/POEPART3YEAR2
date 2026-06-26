757505f21e46e70dc58b0e756c84169741d076d6
ST10466448 <ST10466448@rcconnect.edu.za>
2026-06-26 14:04:07 +0200

    Add files via upload

---
+++ QuizQuestions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POEPART2YEAR2
+{
+    public class QuizQuestion
+    {
+        public string Question { get; set; }
+
+        public string CorrectAnswer { get; set; }
+    }
+}
